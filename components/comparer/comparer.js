d466638e05d81b7c0efecb01cfb7120d7bc7a0fc
Priyanka <priyankab407@gmail.com>
2019-10-11 14:27:14 -0500

    updated comparer/comparer.js

--- components/comparer/comparer.js
+++ components/comparer/comparer.js
@@ -1,17 +1,17 @@
-
+const shortest = (x, y) => { return Math.min(x.length, y.length) }
 
 const comparer = new Vue({
   el: '#comparer',
   data: {
-    guestcomp: 'Emmett',
-    str1: newyork,
-    str2: chicago
+    guestc: 'Emmett',
+    str1: 'newyork',
+    str2: 'chicago'
   },
   computed: {
     shorteststring: function () {
       const i = (this.str1)
       const j = (this.str2)
-      return `${this.guestcomp}, shortest string is ${ Math.min(i.length, j.length)}.`
+      return `${this.guestc}, shortest string length is ${shortest(i, j)}.`
     }
   }
 })
